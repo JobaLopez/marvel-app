2b90081994aa1c65cb6b4b09828573e2732ec9bd
Joba Lopez <jobalc.94@outlook.es>
2023-03-29 17:38:10 +0200

    Created hero-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroGridComponent } from './components/hero-grid/hero-grid.component';
+import { HeroDetailsComponent } from './components/hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroGridComponent
+    HeroGridComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule,
