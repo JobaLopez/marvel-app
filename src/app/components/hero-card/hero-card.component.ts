3cf776a6881500a22db9eacb26306c39c21fbeb6
Joba Lopez <jobalc.94@outlook.es>
2023-03-29 20:43:03 +0200

    Added hero-card html to test

--- src/app/components/hero-card/hero-card.component.ts
+++ src/app/components/hero-card/hero-card.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-hero-card',
+  selector: 'hero-card',
   templateUrl: './hero-card.component.html',
-  styleUrls: ['./hero-card.component.css']
+  styleUrls: ['./hero-card.component.css'],
 })
 export class HeroCardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
