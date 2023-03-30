2aff45b720b1e7c99d3e036effe5f2e56fe69715
Joba Lopez <jobalc.94@outlook.es>
2023-03-30 17:21:38 +0200

    Header changes depending on the active route

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  backButton = false;
+  searchBar = false;
+
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        if (router.url === '/') {
+          this.searchBar = true;
+          this.backButton = false;
+        } else {
+          this.searchBar = false;
+          this.backButton = true;
+        }
+      }
+    });
+  }
+
+  ngOnInit(): void {}
+
   search() {
     console.log('search');
   }
