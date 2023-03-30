a05922337ef7496e49f96e7eabf7e93d287de347
Joba Lopez <jobalc.94@outlook.es>
2023-03-30 12:13:45 +0200

    Testing service

--- src/app/components/hero-grid/hero-grid.component.ts
+++ src/app/components/hero-grid/hero-grid.component.ts
@@ -1,15 +1,36 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HeroServiceService } from 'src/app/services/hero-service.service';
 
 @Component({
   selector: 'app-hero-grid',
   templateUrl: './hero-grid.component.html',
-  styleUrls: ['./hero-grid.component.css']
+  styleUrls: ['./hero-grid.component.css'],
 })
 export class HeroGridComponent implements OnInit {
+  private limit = 30;
+  private offset = 0;
 
-  constructor() { }
+  constructor(private service: HeroServiceService) {}
 
   ngOnInit(): void {
+    this.getHeroes(true);
   }
 
+  getHeroes(firstLoad: boolean, searchName?: string) {
+    const params: HttpParams = new HttpParams({
+      fromObject: { orderBy: 'name', limit: this.limit, offset: this.offset },
+    });
+
+    if (!firstLoad) {
+      this.offset += this.limit;
+    }
+    if (searchName) {
+      params.append('name', searchName);
+    }
+
+    this.service.getHeroes(params).subscribe((gridData) => {
+      console.log(gridData);
+    });
+  }
 }
--- src/app/services/hero-service.service.ts
+++ src/app/services/hero-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroServiceService {
+  url = 'http://gateway.marvel.com/v1/public/';
+  publicKey = '93307aa340a5cc11263bd2a7ff5905cb';
 
-  constructor() { }
+  // https://gateway.marvel.com:443/v1/public/characters?name=ironamn&orderBy=name&limit=30&offset=0&apikey=
+
+  constructor(private http: HttpClient) {}
+
+  getHeroes(params: HttpParams): Observable<ArrayBuffer> {
+    const options = { params };
+    return this.http.get<ArrayBuffer>(this.url, options);
+  }
 }
